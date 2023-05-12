26b61f1fc807a1249bc076fcba437b59cf7732fc
Istinnie <n.istinnie.velonkasina@gmail.com>
2023-05-12 14:08:04 +0400

    add homepage and all components

---
+++ client/src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+  {
+    image: "http://www.videodrive.fr/pub/w2k23.jpg",
+  },
+  {
+    image: "http://www.videodrive.fr/pub/krdld.jpg",
+  },
+  {
+    image: "http://www.videodrive.fr/pub/gowr1.jpg",
+  },
+  {
+    image: "http://www.videodrive.fr/pub/bcld.jpg",
+  },
+  {
+    image: "http://www.videodrive.fr/pub/opo.jpg",
+  },
+];
