3bdaeb7ccb36e31d92ad007500d1a0c10ba02c11
ZephirSD <siegfrieddallery@gmail.com>
2023-05-15 10:28:48 +0400

    Ajout de corsOptions

--- server/app.js
+++ server/app.js
@@ -1,22 +1,26 @@
-// const mongoose = require('mongoose');
 const express = require('express');
 const dbConfig = require('./config/db.config');
 const router = require('./routes/jeuxRoutes');
 const app = express();
 const port = 5000;
+const cors = require("cors");
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Application connecté" });
 });
 
 require('./routes/jeuxRoutes')(app);
 
-// set port, listen for requests
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
