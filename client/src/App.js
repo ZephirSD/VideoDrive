a6a7037ae5ff06f4f10ed41c3cd9ed21fb0430c7
ZephirSD <siegfrieddallery@gmail.com>
2023-05-15 10:26:35 +0400

    Ajout de `fetchJeux` pour afficher tous les jeux à partir de l'API

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style/style.scss";
 import HomePage from "./pages/HomePage";
 import FicheProduit from "./pages/FicheProduit";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from 'axios';
 function App() {
+  const [jeuxListe, setjeuxListe] = useState([]);
+  const fetchJeux = () => {
+    axios.get('http://127.0.0.1:5000/api/jeux')
+      .then(response => setjeuxListe(response.data));
+  }
+  useEffect(() => {
+    fetchJeux();
+  }, []);
   return (
     // <HomePage />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<HomePage jeuxListe={jeuxListe} />}></Route>
         <Route path="/ficheProduit" element={<FicheProduit />}></Route>
       </Routes>
     </BrowserRouter>
