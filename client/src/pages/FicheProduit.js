0a5ba1dbf22422a44b1a1e489c5d8b3bce250f92
ZephirSD <siegfrieddallery@gmail.com>
2023-05-15 11:24:41 +0400

    Ajout de `fetchJeuxId` pour avoir les jeux en fonction de l'id récupéré de l'url `ficheProduit`

--- client/src/pages/FicheProduit.js
+++ client/src/pages/FicheProduit.js
@@ -1,42 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 function FicheProduit() {
+    const [jeuxIdListe, setjeuxIdListe] = useState([]);
+    let paramID = useParams();
+    const fetchJeuxId = async () => {
+       await axios.get(`http://127.0.0.1:5000/api/jeux/${paramID.id}`)
+        .then(response => setjeuxIdListe(response.data));
+    }
+    useEffect(() => {
+        fetchJeuxId();
+    },[]);
   return (
       <>
           <Header/>
           <main className='padMain'>
               <div className="gridInfo">
                   <div className='caseTitreImage'>
-                      <h1 className='titreProduit'>Ys IX Monstrum Nox Deluxe Edition</h1>
+                      <h1 className='titreProduit'>{ jeuxIdListe.nom }</h1>
                       <div className='caseImage'>
-                          {/* <img src="" alt="imageProduit" srcset="" /> */}
+                          <img src={jeuxIdListe.image} alt="imageProduit" srcset="" />
                       </div>
-                      <p className='paraGenre'><span className='libelleGenre'>Genre:&nbsp;</span><span className='texteGenre'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, est.</span></p>
+                      <p className='paraGenre'><span className='libelleGenre'>Genre:&nbsp;</span><span className='texteGenre'>{ jeuxIdListe.genre}</span></p>
                   </div>
                   <div className='caseBtnPrix'>
                       <div className='caseBtn'>
                         <button className="btnPanier">Ajouter au panier&nbsp;<span><FontAwesomeIcon icon={faPlus}/></span></button>
                       </div>
                       <div className="casePrix">
-                          <div className="prix">49,00 €</div>
+                          <div className="prix">{`${jeuxIdListe.prix_neuf} €`}</div>
                           <div className="libellePrix">Prix neuve</div>
                       </div>
                       <div className="casePrix">
-                          <div className="prix">49,00 €</div>
+                          <div className="prix">{`${jeuxIdListe.prix_occasion} €`}</div>
                           <div className="libellePrix">Prix occasion</div>
                       </div>
                       <div className="casePrix">
-                          <div className="prix">49,00 €</div>
+                          <div className="prix">{`${jeuxIdListe.prix_expo} €`}</div>
                           <div className="libellePrix">Prix expo</div>
                       </div>
                   </div>
               </div>
               <div className="paraDescr">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, quisquam modi voluptatem ullam minus quos! Ad, molestias fugit voluptas culpa tenetur omnis modi, rem labore maxime sapiente, debitis eius. Aperiam ipsa ipsum culpa quam sit recusandae quo ratione consequuntur itaque rerum, quas magni excepturi rem esse eveniet inventore aliquam quaerat fugiat neque doloremque nemo quae? Modi, adipisci rem numquam a quisquam ea! Doloribus, adipisci quo.
+                  {jeuxIdListe.description}
               </div>
           </main>
           <Footer/>
